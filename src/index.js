2a2293fa889a5c865d7105ceeb768cf3d2134042
zdanovich89 <zdanovich89@gmail.com>
2019-10-06 23:17:06 +0300

    feat: create function to decode Morse

--- src/index.js
+++ src/index.js
@@ -38,8 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  let arrOneZero = [];
+  for (let i = 0; i < expr.length; i+= 10 ) {
+    let sym = expr.slice(i, (i + 10));
+    arrOneZero.push(sym);
+  } 
+  let arrMorse = [];
+  for (let j = 0; j < arrOneZero.length; j++) {
+   let stringLetter = ''; 
+    for (let k = 0; k < arrOneZero[j].length; k+= 2) {
+      
+      if (arrOneZero[j][k] + arrOneZero[j][k+1] === '00' ) continue;
+      if (arrOneZero[j][k] + arrOneZero[j][k+1] =='10') {
+        stringLetter += '.';
+      }
+      if (arrOneZero[j][k] + arrOneZero[j][k+1] == '11') {
+        stringLetter += '-';
+      }
+      if (arrOneZero[j][k] + arrOneZero[j][k+1] == '**') {
+        stringLetter = ' ';
+      } 
+    }
+    arrMorse.push(stringLetter); 
+  } 
+  let arrLetter = [];
+  for (let m = 0; m < arrMorse.length; m++) {
+    arrLetter.push(MORSE_TABLE[arrMorse[m]]);
+  }
+  let arrLetterSpace = [];
+  for (let n = 0; n < arrLetter.length; n++) {
+      if (arrLetter[n]) {
+      arrLetterSpace.push(arrLetter[n])
+    };
+      if(arrLetter[n] == undefined)
+      arrLetterSpace.push(' ');
+    }
+
+let strExit = arrLetterSpace.join('')
+  return strExit;
+
+  }
+    
+
 
 module.exports = {
     decode
